bb2bc1bdc727d0f1e66ea6fcfdc73fe41551e417
Ivan Vinicius Boneti <42414@UNIDAVI.LOCAL>
2019-03-21 20:28:11 -0300

    banco

---
+++ example/json/config.php
@@ -0,0 +1,104 @@
+<?php
+ini_set('display_errors',1);
+ini_set('display_startup_erros',1);
+error_reporting(E_ALL);
+
+try {
+    $servidor = "localhost"; //localhost, ip ou host
+    $banco = "api_ecommerce"; //nome do banco de dados
+    $usuario = "root"; //usuario
+    $senha = ""; //senha de conexao
+    $pdo = new PDO ("mysql:host=$servidor;dbname=$banco;charset=utf8","$usuario","$senha");
+} catch (PDOException $e) {
+	echo "Erro de Conexão " . $e->getMessage() . "\n";
+	exit;
+}
+
+function slugify($string) {
+    $string = preg_replace('/[\t\n]/', ' ', $string);
+    $string = preg_replace('/\s{2,}/', ' ', $string);
+    $list = array(
+        'Š' => 'S',
+        'š' => 's',
+        'Đ' => 'Dj',
+        'đ' => 'dj',
+        'Ž' => 'Z',
+        'ž' => 'z',
+        'Č' => 'C',
+        'č' => 'c',
+        'Ć' => 'C',
+        'ć' => 'c',
+        'À' => 'A',
+        'Á' => 'A',
+        'Â' => 'A',
+        'Ã' => 'A',
+        'Ä' => 'A',
+        'Å' => 'A',
+        'Æ' => 'A',
+        'Ç' => 'C',
+        'È' => 'E',
+        'É' => 'E',
+        'Ê' => 'E',
+        'Ë' => 'E',
+        'Ì' => 'I',
+        'Í' => 'I',
+        'Î' => 'I',
+        'Ï' => 'I',
+        'Ñ' => 'N',
+        'Ò' => 'O',
+        'Ó' => 'O',
+        'Ô' => 'O',
+        'Õ' => 'O',
+        'Ö' => 'O',
+        'Ø' => 'O',
+        'Ù' => 'U',
+        'Ú' => 'U',
+        'Û' => 'U',
+        'Ü' => 'U',
+        'Ý' => 'Y',
+        'Þ' => 'B',
+        'ß' => 'Ss',
+        'à' => 'a',
+        'á' => 'a',
+        'â' => 'a',
+        'ã' => 'a',
+        'ä' => 'a',
+        'å' => 'a',
+        'æ' => 'a',
+        'ç' => 'c',
+        'è' => 'e',
+        'é' => 'e',
+        'ê' => 'e',
+        'ë' => 'e',
+        'ì' => 'i',
+        'í' => 'i',
+        'î' => 'i',
+        'ï' => 'i',
+        'ð' => 'o',
+        'ñ' => 'n',
+        'ò' => 'o',
+        'ó' => 'o',
+        'ô' => 'o',
+        'õ' => 'o',
+        'ö' => 'o',
+        'ø' => 'o',
+        'ù' => 'u',
+        'ú' => 'u',
+        'û' => 'u',
+        'ý' => 'y',
+        'ý' => 'y',
+        'þ' => 'b',
+        'ÿ' => 'y',
+        'Ŕ' => 'R',
+        'ŕ' => 'r',
+        '/' => '-',
+        ' ' => '-',
+        '.' => '-',
+    );
+
+    $string = strtr($string, $list);
+    $string = preg_replace('/-{2,}/', '-', $string);
+    $string = strtolower($string);
+
+    return $string;
+}
