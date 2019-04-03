88fc34cf7a2a0e4d8821e94657d29c17aaa28781
unknown <lucasgsebold1@gmail.com>
2019-04-03 20:07:10 -0300

    Descrição Produto OK

--- app/js/produto.js
+++ app/js/produto.js
@@ -23,7 +23,28 @@ $(document).ready(function() {
     
 function preencherProduto(dados) {
     $.each( dados, function ( key, val )  {
-       // $(".foto").html(val.imagem);
+        var img = '';
+            
+        if(dados != ""){
+            var iCodigoCategoria = val.catcodigo;
+        }
+
+        if(iCodigoCategoria == 1){
+           img = "<img src='https://i.zst.com.br/thumbs/45/12/19/239625442.jpg' alt='fogão'>" ;
+        }
+
+        if(iCodigoCategoria == 2){
+            img = "<img src='https://images-submarino.b2w.io/produtos/01/00/item/7440/9/7440973SZ.jpg' alt='geladeira'>" ;
+        }
+
+        if(iCodigoCategoria == 3){
+            img = "<img src='https://a-static.mlcdn.com.br/618x463/forno-eletrico-philco-46l-com-timer/magazineluiza/216448600/295228559ec6ff61dfb31ada1b2370d0.jpg' alt='forno'>" ;
+        }
+
+        if(iCodigoCategoria == 4){
+            img = "<img src='https://images-na.ssl-images-amazon.com/images/I/81NNgzboHhL._SX425_.jpg' alt='microondas'>" ;
+        }
+        $(".foto").html(img);
         $(".descricao").html("<p>"+val.prodescricao+"</p><p class='text-center'><a href='carrinho.html?op=add' class='btn btn-danger btn-lg'>Comprar</a></p>");
         $("h1").html(val.prodescricao);
     })
