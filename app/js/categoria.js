5e48531b4627a5a49e4aa0faf04ce4a70dc1167a
Ivan <ivanvincius88@unidavi.edu.br>
2019-04-03 19:43:21 -0300

    if com as imagens

--- app/js/categoria.js
+++ app/js/categoria.js
@@ -28,21 +28,36 @@ $(document).ready(function() {
 
     //funcao para preencher os produtos
     function preencher( dados ) {
+        var img = '';
 
-        $.each( dados, function( key, val ) {
-            if(val.catcodigo == 1) {
-                var img = "<img src='https://casaevideodigital.vteximg.com.br/arquivos/ids/158923-1000-1000/1462490c.jpg?v=636692645095430000'></img>";
-            } else if(val.catcodigo == 2) {
-                var img = "<img src='https://casaevideodigital.vteximg.com.br/arquivos/ids/158923-1000-1000/1462490c.jpg?v=636692645095430000'></img>";
-            }
-            else if(val.catcodigo == 3) {
-                var img = "<img src='https://casaevideodigital.vteximg.com.br/arquivos/ids/158923-1000-1000/1462490c.jpg?v=636692645095430000'></img>";
-            }
-            else if(val.catcodigo == 4) {
-                var img = "<img src='https://casaevideodigital.vteximg.com.br/arquivos/ids/158923-1000-1000/1462490c.jpg?v=636692645095430000'></img>";
-            }
-            //adicionar os itens no .row
-            $(".row").append("<div class='col-md-6 col-sm-6 text-center'><div class='thumbnail'>"+ img + "<p>"+val.prodescricao+"</p><p class='valor'>"+val.provalor+"</p><a href='produto.html?id="+val.procodgio+"' class='btn btn-danger'>Detalhes</a></div></div>");
+
+        if(dados != ""){
+            var iCodigoCategoria = dados[1]['catcodigo'];
+        }
+
+        if(iCodigoCategoria == 1){
+           img = "<img src='https://i.zst.com.br/thumbs/45/12/19/239625442.jpg' alt='fogão'>" ;
+        }
+
+        if(iCodigoCategoria == 2){
+            img = "<img src='https://images-submarino.b2w.io/produtos/01/00/item/7440/9/7440973SZ.jpg' alt='geladeira'>" ;
+        }
+
+        if(iCodigoCategoria == 3){
+            img = "<img src='https://a-static.mlcdn.com.br/618x463/forno-eletrico-philco-46l-com-timer/magazineluiza/216448600/295228559ec6ff61dfb31ada1b2370d0.jpg' alt='forno'>" ;
+        }
+
+        if(iCodigoCategoria == 4){
+            img = "<img src='https://images-na.ssl-images-amazon.com/images/I/81NNgzboHhL._SX425_.jpg' alt='microondas'>" ;
+        }
+
+        $.each( dados, function( id = key, val ) {
+            dados[id]['catcodigo']
+
+
+
+            
+            $(".row").append("<div class='col-md-6 col-sm-6 text-center'><div class='thumbnail'>" + img + "<p>"+val.prodescricao+"</p><p class='valor'>"+val.provalor+"</p><a href='produto.html?id="+val.procodgio+"' class='btn btn-danger'>Detalhes</a></div></div>");
         })
         $("#msg").html("");
     }
